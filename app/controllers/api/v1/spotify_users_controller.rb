0c8f3acab9acc0281d88fa58b65de40930132bc5
farverio <github email address>
2018-11-06 10:35:12 -0600

    updated spotify users controller

--- app/controllers/api/v1/spotify_users_controller.rb
+++ app/controllers/api/v1/spotify_users_controller.rb
@@ -1,8 +1,10 @@
 class Api::V1::SpotifyUsersController < ApplicationController
+  skip_before_action :authorized, only: [:create]
+  
   def create
     venue = Venue.find_by(slug: spotify_user_params[:venue_slug])
     
-    SpotifyApi.get_all_spotify_info(spotify_user_params[:access_token], venue)
+    result = SpotifyApi.get_all_spotify_info(spotify_user_params[:access_token], venue)
   end
   
   private
