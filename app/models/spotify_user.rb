391de28c0333dfd825d47b70c76efade1771f392
farverio <github email address>
2018-11-06 12:16:08 -0600

    move top artists method to Venue Spotify User model

--- app/models/spotify_user.rb
+++ app/models/spotify_user.rb
@@ -1,8 +1,4 @@
 class SpotifyUser < ApplicationRecord
   has_many :venues, through: :venue_spotify_users
   has_many :artists, through: :spotify_user_artists
-
-  def get_top_artists
-    SpotifyUserArtist.all.select{|spotify_user_artist| spotify_user_artist.user == self}
-  end
 end
--- app/models/venue_spotify_user.rb
+++ app/models/venue_spotify_user.rb
@@ -1,4 +1,8 @@
 class VenueSpotifyUser < ApplicationRecord
   belongs_to :spotify_user
   belongs_to :venue
+  
+  def get_top_artists
+    SpotifyUserArtist.all.select{|spotify_user_artist| spotify_user_artist.user == self}
+  end
 end
