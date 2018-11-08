3278261f1ba159487581841279c2876e329e8042
farverio <stephen@farver.io>
2018-11-07 20:26:53 -0600

    updated user growth query

--- app/controllers/api/v1/spotify_users_controller.rb
+++ app/controllers/api/v1/spotify_users_controller.rb
@@ -1,6 +1,20 @@
 class Api::V1::SpotifyUsersController < ApplicationController
   skip_before_action :authorized, only: [:create]
   
+  def index 
+    venue = current_venue
+    
+    users_growth = venue.spotify_users.reduce({}) do |total, user|
+      date = user.created_at.to_date
+      
+      total[date] ? total[date] += 1 : total[date] = 1
+      
+      total
+    end
+
+    users_growth  
+  end
+  
   def create
     venue = Venue.find_by(slug: spotify_user_params[:venue_slug])
     
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       post '/login', to: 'auth#create'
 
-      resources :spotify_users, only: [:create]
+      resources :spotify_users, only: [:create, :index]
       resources :venues, only: [:create]
+      
       get '/top_artists', to: 'venues#top_artists'
       get '/top_genres', to: 'venues#top_genres'
     end
