30ffe27a449ce67dd089bd7e01263d7b7485c7df
farverio <stephen@farver.io>
2018-11-08 11:00:28 -0600

    fixed bitly api bug

--- app/models/bitly_api.rb
+++ app/models/bitly_api.rb
@@ -4,4 +4,5 @@ class BitlyApi
     # NOTE: need to pass in campaign slug to set Spotify state to this
     # URL NEEDS TO BE THE SAME FOR EACH CLIENT OTHERWISE CAN'T BE FORWARDED BY SPOTIFY
     # HOW TO MAKE CALLBACK LANDING PAGE DISPLAY DIFFERENT THINGS BASED ON SPOTIFY STATE
+  end
 end
